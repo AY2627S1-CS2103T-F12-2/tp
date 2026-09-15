63da5f37816ed57ee152d35a7152526704561b9f
claire p <clairephay@gmail.com>
2026-09-15 22:37:31 +0800

    Add ability to parse input

--- src/main/java/seedu/address/logic/commands/RemarkCommand.java
+++ src/main/java/seedu/address/logic/commands/RemarkCommand.java
@@ -22,9 +22,6 @@ public class RemarkCommand extends Command {
             + "Example: " + COMMAND_WORD + " 1 "
             + "r/ Likes to swim.";
 
-    public static final String MESSAGE_NOT_IMPLEMENTED_YET =
-            "Remark command not implemented yet";
-
     public static final String MESSAGE_ARGUMENTS = "Index: %1$d, Remark: %2$s";
 
     private final Index index;
@@ -65,4 +62,3 @@ public class RemarkCommand extends Command {
     }
 }
 
-}
