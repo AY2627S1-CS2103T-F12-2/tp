832116c809e6659be9aa7c0f432c132a52cd1fbb
Papangkorn-Pann <e1524004@u.nus.edu>
2026-09-25 11:49:06 +0800

    Add remark command and appropriate tests

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -9,6 +9,10 @@ import static java.util.Objects.requireNonNull;
 public class Remark {
     public final String value;
 
+    /**
+     * Constructor: creates Remark object holding String value for its remark
+     * @param remark
+     */
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
